b108c6f66ebffeec7dd48cc1a6992516fa6ad70a
Etsybt <hmidouchnouhaila2@gmail.com>
2023-12-22 21:35:28 +0000

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,23 @@
+# Install Python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# Install pip for Python 3.8
 package { 'python3-pip':
   ensure => installed,
+  require => Package['python3.8'],
 }
 
-package { 'python3-venv':
-  ensure => installed,
-}
-
+# Install Flask 2.1.0 using pip
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin',
-  require => [Package['python3-pip'], Package['python3-venv']],
+  command => '/usr/bin/python3.8 -m pip install --force-reinstall Flask==2.1.0',
+  require => Package['python3-pip'],
 }
+
+# Install Werkzeug 2.1.1 using pip
+exec { 'install_werkzeug':
+  command => '/usr/bin/python3.8 -m pip install --force-reinstall Werkzeug==2.1.1',
+  require => Package['python3-pip'],
+}
+
