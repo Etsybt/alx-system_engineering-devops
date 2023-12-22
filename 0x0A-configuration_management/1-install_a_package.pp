6fbce9f0d8e032f699a4c8e4b48ca51fe7ac5b01
Etsybt <hmidouchnouhaila2@gmail.com>
2023-12-22 19:46:24 +0000

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#installs flask from pip3
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-venv':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin',
+  require => [Package['python3-pip'], Package['python3-venv']],
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 #a manifest that kills a process named killmenow.
 
 exec { 'killmenow':
-  command     => 'pkill killmenow',
-  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-  refreshonly => true,
+  path        => '/usr/bin/',
+  command     => 'pkill -f killmenow',
 }
