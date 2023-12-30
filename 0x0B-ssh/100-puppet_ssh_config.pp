da2ab5078c5a4fccb64cc15e17a9772050adfb74
Etsybt <hmidouchnouhaila2@gmail.com>
2023-12-30 16:01:15 +0000

    update

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#Puppet script to make changes to the configuration file.
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/sshd_config',
+  line => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
+}
