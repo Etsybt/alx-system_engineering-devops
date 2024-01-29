d1ceeedefcdf8dd69d81c839801c0620a9438059
Etsybt <hmidouchnouhaila2@gmail.com>
2024-01-29 17:48:38 +0000

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,43 +1,24 @@
 #!/usr/bin/python3
-
-"""
-This Python script utilizes a REST API to fetch information
-regarding the progress of a given employee's TODO list.
-"""
-
-from requests import get
-from sys import argv
+"""Retrieves data from REST API and exports it in CSV format"""
 
 if __name__ == "__main__":
-    # Fetching TODOs and users data
-    todos_response = get('https://jsonplaceholder.typicode.com/todos/')
-    todos_data = todos_response.json()
-    users_response = get('https://jsonplaceholder.typicode.com/users')
-    users_data = users_response.json()
 
-    # Finding the employee by ID
-    employee_name = None
-    for user in users_data:
-        if user.get('id') == int(argv[1]):
-            employee_name = user.get('name')
-            break
+    import csv
+    import requests
+    import sys
 
-    if employee_name:
-        # Filtering tasks owned by the employee
-        tasks = []
-        for todo in todos_data:
-            if todo.get('userId') == int(argv[1]):
-                tasks.append(todo)
+    employee_id = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(employee_id))
+    employee_name = user.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-        # Writing tasks to CSV file
-        filename = "{}.csv".format(argv[1])
-        with open(filename, 'w') as csvfile:
-            for task in tasks:
-                csvfile.write('"{}","{}","{}","{}"\n'.format(
-                    task['userId'], employee_name, str(
-                        task['completed']), task['title']
-                ))
-
-        print("Data exported to", filename)
-    else:
-        print("Employee not found.")
+    filename = employee_id + '.csv'
+    with open(filename, mode='w') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in todos.json():
+            if task.get('userId') == int(employee_id):
+                writer.writerow([employee_id, employee_name, str(
+                    task.get('completed')),
+                                 task.get('title')])
