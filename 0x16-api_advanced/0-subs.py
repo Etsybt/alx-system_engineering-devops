fde174e9953a830395c15fdd3f02a6c4dc13ed53
Etsybt <hmidouchnouhaila2@gmail.com>
2024-02-14 13:37:05 +0000

    update

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+api
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API
+
+    Args:
+    - subreddit: The name of the subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyBot/1.0"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
